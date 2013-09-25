ebc55614b2ff10945bd758f903a1fc416f12cbff
Murtuza <murtuzafirst@gmail.com>
2013-09-25 13:07:38 +0530

    fixes

--- scraper.js
+++ scraper.js
@@ -1,22 +1,21 @@
+notifications = {}
 count = parseInt($(".count").first().html());
-
 var animateFn = function () {
 	if ( count > 0 ) {
-		console.log("going down ");
-		$("body").animate({ scrollTop: $(document).height() }, {
+		setTimeout(function(){
+			$("body").animate({ scrollTop: $(document).height() }, {
 			complete: function () {
 				count -= 15;
 				animateFn();
 		  }
-		});
+		});	
+		}, 100)
+		
 	} else {
 		fn2();
 	}
 }
-
 var fn2 = function () {
-	notification = {}
-	console.log("Outside the while loop");
 	$("body").animate({ scrollTop: 0 }, "slow");
 	$('div.pagedlist_item li.unseen .notification_text').each(function(){
 		a_tags = $(this).find("a");
@@ -31,8 +30,9 @@ var fn2 = function () {
 		}	
 	});
 	console.log(notifications);
+	chrome.runtime.sendMessage( notifications )
+	//chrome.extension.sendMessage( notifications )
 }
-// var port = chrome.runtime.connect({name: "notification_relay"});
-// port.postMessage(notifications);
 animateFn();
+
 
