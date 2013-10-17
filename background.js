62e9becbb78924377dff022f1a087c199118f1cd
Murtuza <murtuzafirst@gmail.com>
2013-10-17 16:38:18 +0530

    working version, for both questions and answers

--- background.js
+++ background.js
@@ -1,38 +1,3 @@
-console.log("machie!");
 chrome.browserAction.onClicked.addListener(function (tab){
-  alert("hello hai ji");
   chrome.tabs.executeScript(tab.id, {file: 'scraper.js'});
-});
-var notifications;
-chrome.extension.onMessage.addListener(function (request) {
-  // var newURL = "background.html";
-  // var tabId = chrome.tabs.create({ url: newURL},function(tab){
-    // tabId = tab.id;
-    var data = "";
-    notifications = request;
-    for (var topic in notifications){
-      if (notifications.hasOwnProperty(topic)) {
-        for(note in notifications[topic]){
-          data += '<div>'+note+'</div>';
-        }
-      }
-    }
-    alert(data);
-    document.body.innerText = "hello";
-    // chrome.tabs.onUpdated.addListener(function(tId, changeInfo, tab) {
-      // if (tId === tabId && changeInfo.status === 'complete') {
-          // document.body.innerText = "God _/\_";
-        // chrome.tabs.executeScript(tabId, {
-        //   code: 'document.body.innerText = "hello";'
-        // });
-      // }
-      // if (tId === tabId && changeInfo.status === 'complete') {
-      //   alert("innertext: " + document.body.innerText);
-      //   chrome.tabs.executeScript(tabId, {
-      //     code: 'document.body.innerText = "hello";'
-      //   });
-      // }
-    // });  
-  // });
-  
 });
--- scraper.js
+++ scraper.js
@@ -1,4 +1,5 @@
 notifications = {}
+notifications_answers = {}
 count = parseInt($(".count").first().html());
 var animateFn = function () {
 	if ( count > 0 ) {
@@ -19,43 +20,90 @@ var fn2 = function () {
 	$("body").animate({ scrollTop: 0 }, "slow");
 	$('div.pagedlist_item li.unseen .notification_text').each(function(){
 		a_tags = $(this).find("a");
-		if($(a_tags[0]).attr('arg') === undefined){}
+		if($(a_tags[0]).attr('arg') === undefined){
+			if( $(a_tags[1]).attr('arg') === undefined ){
+			}
+			else{
+				obj = eval("(" + $(a_tags[1]).attr('arg') + ")");
+				if(obj.hasOwnProperty("aid")){
+					topic = a_tags[1].href.split("http://www.quora.com/")[1].split("?")[0];
+					topic = topic.split("/answer")[0]
+				  if( notifications_answers[topic] === undefined ){
+				 	 notifications_answers[topic] = [];
+				  }
+				  note = a_tags[1].href.split("http://www.quora.com/")[1];
+				  notifications_answers[topic].push(note);
+				}
+			}
+		}
 		else{
-		  topic = a_tags[1].href.split("http://www.quora.com/")[1].split("?")[0];
-		  if( notifications[topic] === undefined ){
-		 	 notifications[topic] = [];
-		  }
-		  note = a_tags[0].href.split("http://www.quora.com/")[1].split("?")[0];
-		  notifications[topic].push(note);
-		}	
+			if($(a_tags[0]).attr('arg')){
+				obj0 = eval("(" + $(a_tags[0]).attr('arg') + ")");
+			}
+			if($(a_tags[1]).attr('arg')){
+				obj1 = eval("(" + $(a_tags[1]).attr('arg') + ")");
+			}
+			if(obj0.hasOwnProperty("qid")){
+			  topic = a_tags[1].href.split("http://www.quora.com/")[1].split("?")[0];
+			  if( notifications[topic] === undefined ){
+			 	 notifications[topic] = [];
+			  }
+			  note = a_tags[0].href.split("http://www.quora.com/")[1];
+			  console.log("Questions ::" + topic + " :: " + note);
+			  notifications[topic].push(note);
+			}else if(obj0.hasOwnProperty("aid")){
+				topic = a_tags[0].href.split("http://www.quora.com/")[1].split("?")[0];
+				console.log(topic);
+			  if( notifications_answers[topic] === undefined ){
+			 	 notifications_answers[topic] = [];
+			  }
+			  note = a_tags[0].href.split("http://www.quora.com/")[1];
+			  notifications_answers[topic].push(note);
+			}
+		}
 	});
-	// chrome.runtime.sendMessage( notifications )
-	// update_url();
+	console.log(notifications);
+	console.log("now answwers");
+	console.log(notifications_answers);
 	build_web_page(notifications);
-	//chrome.extension.sendMessage( notifications )
 }
 
 animateFn();
-
-function update_url(){
-	chrome.tabs.getSelected(function (tab) {
-	  var tabUrl = encodeURIComponent(tab.url);
-	  var tabTitle = encodeURIComponent(tab.title);
-	  chrome.tabs.update(tab.id, {url: "chrome-extensions://test/hello.html"});
-	});
-}
-
+var style = "<style type='text/css'>\
+li {color:blue;text-align:center}\
+ul {font-size:20px;}\
+</style>"
 function build_web_page(notifications_object){
-	data = "";
+	data = "QUESTIONS :::";
+	data += style;
 	for (var topic in notifications){
     if (notifications.hasOwnProperty(topic)) {
-    	data += '<div>'+ topic +'</div>';
     	var list_obj = notifications[topic];
+    	data += '<ul>'+ topic + '  (' + list_obj.length + ')';
       for(var i=0; i< list_obj.length; i++){
-        data += '<div>'+list_obj[i]+'</div>';
+        data += '<li>'+list_obj[i]+'</li>';
       }
     }
-    data += "<br/><br/><br/><br/><br/><br/>";
+    data += "</ul><br/><br/><br/><br/><br/><br/>";
+  }
+  data += "<br/><br/><br/><br/>ANSWERS :::<br/><br/>";
+	data += style;
+	for (var topic in notifications_answers){
+    if (notifications_answers.hasOwnProperty(topic)) {
+    	var list_obj = notifications_answers[topic];
+    	data += '<ul>'+ topic + '  (' + list_obj.length + ')';
+      for(var i=0; i< list_obj.length; i++){
+        data += '<li>'+list_obj[i]+'</li>';
+      }
+    }
+    data += "</ul><br/><br/><br/><br/><br/><br/>";
   }
   document.body.innerHTML = data;
+}
+
+function notification_killer(array){
+	for(var i=0; i < array.length; i++){
+		console.log("clearing" + array[i] );
+		$.ajax({ url: array[i] });
+	}
 }
